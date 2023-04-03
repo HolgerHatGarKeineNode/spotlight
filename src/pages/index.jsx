b1647c83fd71d4d9ea8550e8ae8cd81b2fc64c32
HolgerHatGarKeineNode <fsociety.mkv@pm.me>
2023-04-03 23:42:22 +0200

    title changed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -354,7 +354,7 @@ export default function Home ({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software developer, founder, and ₿itcoiner.
+            Software developer and founder.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Benjamin, a software developer and entrepreneur based in Füssen. I’m the founder and CEO of Affekt
